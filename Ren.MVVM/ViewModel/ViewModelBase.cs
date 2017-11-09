6a8b2db4956c4ee54dcc24f9628558a09363c004
korewa <mizmuxx@gmail.com>
2017-11-09 23:52:40 +0100

    add #Ren.MVVM

---
+++ Ren.MVVM/ViewModel/ViewModelBase.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Ren.MVVM.ViewModel
+{
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = default(string)) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        protected virtual bool SetProperty<T>(ref T setter, T value, [CallerMemberName] string propertyName = default(string))
+        {
+            if (EqualityComparer<T>.Default.Equals(setter, value)) return false;
+            setter = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+    }
+}
